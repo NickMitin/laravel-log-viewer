caeac8e973193cb2cab312ff2a233efb222697e6
Николай Митин <nick.mitin@gmail.com>
2020-03-05 21:27:22 +0300

    Rewritten representation

--- src/Rap2hpoutre/LaravelLogViewer/LaravelLogViewer.php
+++ src/Rap2hpoutre/LaravelLogViewer/LaravelLogViewer.php
@@ -8,279 +8,461 @@ namespace Rap2hpoutre\LaravelLogViewer;
  */
 class LaravelLogViewer
 {
-    /**
-     * @var string file
-     */
-    private $file;
+	/**
+	 * @var string file
+	 */
+	private $file;
 
-    /**
-     * @var string folder
-     */
-    private $folder;
+	/**
+	 * @var string folder
+	 */
+	private $folder;
 
-    /**
-     * @var string storage_path
-     */
-    private $storage_path;
+	/**
+	 * @var string storage_path
+	 */
+	private $storage_path;
 
-    /**
-     * Why? Uh... Sorry
-     */
-    const MAX_FILE_SIZE = 52428800;
+	/**
+	 * Why? Uh... Sorry
+	 */
+	const MAX_FILE_SIZE = 52428800;
 
-    /**
-     * @var Level level
-     */
-    private $level;
+	/**
+	 * @var Level level
+	 */
+	private $level;
 
-    /**
-     * @var Pattern pattern
-     */
-    private $pattern;
+	/**
+	 * @var Pattern pattern
+	 */
+	private $pattern;
 
-    /**
-     * LaravelLogViewer constructor.
-     */
-    public function __construct()
-    {
-        $this->level = new Level();
-        $this->pattern = new Pattern();
-        $this->storage_path = function_exists('config') ? config('logviewer.storage_path', storage_path('logs')) : storage_path('logs');
+	/**
+	 * LaravelLogViewer constructor.
+	 */
+	public function __construct()
+	{
+		$this->level = new Level();
+		$this->pattern = new Pattern();
+		$this->storage_path = function_exists('config') ? config('logviewer.storage_path', storage_path('logs')) : storage_path('logs');
+	}
 
-    }
+	/**
+	 * @param string $date
+	 */
+	private function prepareJSON($json)
+	{
+		$json = preg_replace('~\n~', '\n', $json);
+		return $json;
+	}
 
-    /**
-     * @param string $folder
-     */
-    public function setFolder($folder)
-    {
-        if (app('files')->exists($folder)) {
-            $this->folder = $folder;
-        }
-        if(is_array($this->storage_path)) {
-            foreach ($this->storage_path as $value) {
-                $logsPath = $value . '/' . $folder;
-                if (app('files')->exists($logsPath)) {
-                    $this->folder = $folder;
-                    break;
-                }
-            }
-        } else {
-            if ($this->storage_path) {
-                $logsPath = $this->storage_path . '/' . $folder;
-                if (app('files')->exists($logsPath)) {
-                    $this->folder = $folder;
-                }
-            }
-        }
-    }
+	private function detectStackLineType($file_name)
+	{
+		if (mb_stripos($file_name, '/vendor/') !== false) {
+			return 'vendor';
+		}
+		return 'user';
+	}
 
-    /**
-     * @param string $file
-     * @throws \Exception
-     */
-    public function setFile($file)
-    {
-        $file = $this->pathToLogFile($file);
+	/**
+	 * @param string $date
+	 */
+	private function splitStackIntoLines($stack_trace)
+	{
+		return preg_split('~\s*\r?\n\s*~m', $stack_trace, -1, PREG_SPLIT_NO_EMPTY);
+	}
 
-        if (app('files')->exists($file)) {
-            $this->file = $file;
-        }
-    }
+	/**
+	 * @param string $date
+	 */
+	private function formatLogDate($date)
+	{
+		$date = trim($date);
+		return $date;
+	}
 
-    /**
-     * @param string $file
-     * @return string
-     * @throws \Exception
-     */
-    public function pathToLogFile($file)
-    {
+	/**
+	 * @param string $value
+	 */
+	private function formatLogValue($value)
+	{
+		$value = trim($value);
+		return $value;
+	}
 
-        if (app('files')->exists($file)) { // try the absolute path
-            return $file;
-        }
-        if (is_array($this->storage_path)) {
-            foreach ($this->storage_path as $folder) {
-                if (app('files')->exists($folder . '/' . $file)) { // try the absolute path
-                    $file = $folder . '/' . $file;
-                    break;
-                }
-            }
-            return $file;
-        }
+	/**
+	 * @param string $level
+	 */
+	private function formatLogLevel($level)
+	{
+		$level = $this->formatLogValue($level);
+		$level = mb_convert_case($level, MB_CASE_LOWER);
+		return $level;
+	}
 
-        $logsPath = $this->storage_path;
-        $logsPath .= ($this->folder) ? '/' . $this->folder : '';
-        $file = $logsPath . '/' . $file;
-        // check if requested file is really in the logs directory
-        if (dirname($file) !== $logsPath) {
-            throw new \Exception('No such log file');
-        }
-        return $file;
-    }
+	/**
+	 * @param string $text
+	 */
+	private function stripDocumentRootFromText($text)
+	{
+		$document_root = base_path();
+		return str_ireplace($document_root, '', $text);
+	}
 
-    /**
-     * @return string
-     */
-    public function getFolderName()
-    {
-        return $this->folder;
-    }
+	/**
+	 * @param string $text
+	 */
+	private function formatText($text)
+	{
+		$text = $this->stripDocumentRootFromText($text);
+		$text = $this->formatLogValue($text);
+		return $text;
+	}
 
-    /**
-     * @return string
-     */
-    public function getFileName()
-    {
-        return basename($this->file);
-    }
+	/**
+	 * @param string $stack_line
+	 */
+	private function splitStackLine($stack_line)
+	{
+		$stack_line_pattern = $this->pattern->getPattern('stack_line');
+		if (preg_match($stack_line_pattern, $stack_line, $matches)) {
+			array_shift($matches);
+			return $matches;
+		}
+		return [0, '', '', 0, ''];
+	}
 
-    /**
-     * @return array
-     */
-    public function all()
-    {
-        $log = array();
+	/**
+	 * @param array $block_storage
+	 * @param string $block_type
+	 * @param string $stack_line
+	 * @param int $block_number
+	 */
+	private function addLineToBlock($block_storage, $block_type, $stack_line, $block_number)
+	{
+		$key = $block_type . '_' . $block_number;
+		if (!array_key_exists($key, $block_storage)) {
+			$block_storage[$key] = (object) ['type' => $block_type, 'number' => $block_number, 'lines' => []];
+		}
+		$block_storage[$key]->lines[] = $stack_line;
+		return $block_storage;
+	}
 
-        if (!$this->file) {
-            $log_file = (!$this->folder) ? $this->getFiles() : $this->getFolderFiles();
-            if (!count($log_file)) {
-                return [];
-            }
-            $this->file = $log_file[0];
-        }
+	/**
+	 * @param array $block_storage
+	 * @param string $stack_line
+	 * @param int $block_number
+	 */
+	private function addLineToUserBlock($block_storage, $stack_line, $block_number)
+	{
+		return $this->addLineToBlock($block_storage, 'user', $stack_line, $block_number);
+	}
+	private function addLineToVendorBlock($block_storage, $stack_line, $block_number)
+	{
+		return $this->addLineToBlock($block_storage, 'vendor', $stack_line, $block_number);
+	}
 
-        $max_file_size = function_exists('config') ? config('logviewer.max_file_size', self::MAX_FILE_SIZE) : self::MAX_FILE_SIZE;
-        if (app('files')->size($this->file) > $max_file_size) {
-            return null;
-        }
+	/**
+	 * @param string $error_text
+	 */
+	private function formatStackTrace($stack_trace)
+	{
+		$stack_trace = $this->prepareJSON($stack_trace);
+		$json_data = json_decode($stack_trace);
+		if (!isset($json_data->exception)) {
+			return $stack_trace;
+		}
+		$stack_lines = $this->splitStackIntoLines($json_data->exception);
+		$output = [];
+		$skip_lines = ['[stacktrace]'];
+		$block_counters = [
+			'vendor' => 0,
+			'user' => 0
+		];
+		$save_block = '';
+		$line_type = 'vendor';
+		foreach ($stack_lines as $line_number => $stack_line) {
+			if (in_array($stack_line, $skip_lines)) {
+				continue;
+			}
+			$stack_line = $this->stripDocumentRootFromText($stack_line);
+			if ($line_number == 0) {
+				$line_type = 'user';
+				continue;
+			} else {
+				list($stack_position, $internal, $file_name, $file_line_number, $code_line) = $this->splitStackLine($stack_line);
+				if ($internal) {
+					$line_type = 'user';
+					$stack_line  = '<div class="user code-line">' . $code_line . '</div>';
+				} else {
+					if (!$file_line_number) {
+						continue;
+					}
+					$line_type = $this->detectStackLineType($file_name);
+					$stack_line = '<div class="' . $line_type . ' file-name">' . $file_name . ' at line ' . $file_line_number . '</div><div class="' . $line_type . ' code-line">' . $code_line . '</div>';
+				}
+			}
 
-        $file = app('files')->get($this->file);
+			if ($save_block && $save_block != $line_type) {
+				$block_counters[$line_type]++;
+			}
+			$save_block = $line_type;
+			$output = $this->addLineToBlock($output, $line_type, $stack_line, $block_counters[$line_type]);
+		}
+		$html_output = '';
+		foreach ($output as $block) {
+			switch ($block->type) {
+				case 'user':
+					$html_output .= '<ul class="stack-trace user">';
+					foreach ($block->lines as $line) {
+						$html_output .= '<li>' . $line . '</li>';
+					}
+					$html_output .= '</ul>';
+					break;
+				case 'vendor':
+					$line_count = count($block->lines);
+					if ($line_count < 3) {
+						$html_output .= '<ul class="stack-trace vendor">';
+						foreach ($block->lines as $line) {
+							$html_output .= '<li>' . $line . '</li>';
+						}
+						$html_output .= '</ul>';
+					} else {
+						$html_output .= '<ul class="stack-trace vendor">';
+						$html_output .= '<li>' . $block->lines[0] . '</li>';
+						$html_output .= '</ul>';
+						$html_output .= '<div class="toggle-block" data-type="' . $block->type . '" data-number="' . $block->number . '">Show ' . ($line_count - 1) . ' more ↓</div>';
+						$html_output .= '<ul class="stack-trace vendor hidden" data-type="' . $block->type . '" data-number="' . $block->number . '">';
+						for ($i = 1; $i < $line_count; $i++) {
+							$html_output .= '<li>' . $block->lines[$i] . '</li>';
+						}
+						$html_output .= '</ul>';
+					}
+					break;
+			}
+		}
+		return $html_output;
+	}
 
-        preg_match_all($this->pattern->getPattern('logs'), $file, $headings);
+	/**
+	 * @param string $folder
+	 */
+	public function setFolder($folder)
+	{
+		if (app('files')->exists($folder)) {
+			$this->folder = $folder;
+		}
+		if (is_array($this->storage_path)) {
+			foreach ($this->storage_path as $value) {
+				$logsPath = $value . '/' . $folder;
+				if (app('files')->exists($logsPath)) {
+					$this->folder = $folder;
+					break;
+				}
+			}
+		} else {
+			if ($this->storage_path) {
+				$logsPath = $this->storage_path . '/' . $folder;
+				if (app('files')->exists($logsPath)) {
+					$this->folder = $folder;
+				}
+			}
+		}
+	}
 
-        if (!is_array($headings)) {
-            return $log;
-        }
+	/**
+	 * @param string $file
+	 * @throws \Exception
+	 */
+	public function setFile($file)
+	{
+		$file = $this->pathToLogFile($file);
 
-        $log_data = preg_split($this->pattern->getPattern('logs'), $file);
+		if (app('files')->exists($file)) {
+			$this->file = $file;
+		}
+	}
 
-        if ($log_data[0] < 1) {
-            array_shift($log_data);
-        }
+	/**
+	 * @param string $file
+	 * @return string
+	 * @throws \Exception
+	 */
+	public function pathToLogFile($file)
+	{
 
-        foreach ($headings as $h) {
-            for ($i = 0, $j = count($h); $i < $j; $i++) {
-                foreach ($this->level->all() as $level) {
-                    if (strpos(strtolower($h[$i]), '.' . $level) || strpos(strtolower($h[$i]), $level . ':')) {
+		if (app('files')->exists($file)) { // try the absolute path
+			return $file;
+		}
+		if (is_array($this->storage_path)) {
+			foreach ($this->storage_path as $folder) {
+				if (app('files')->exists($folder . '/' . $file)) { // try the absolute path
+					$file = $folder . '/' . $file;
+					break;
+				}
+			}
+			return $file;
+		}
 
-                        preg_match($this->pattern->getPattern('current_log', 0) . $level . $this->pattern->getPattern('current_log', 1), $h[$i], $current);
-                        if (!isset($current[4])) {
-                            continue;
-                        }
+		$logsPath = $this->storage_path;
+		$logsPath .= ($this->folder) ? '/' . $this->folder : '';
+		$file = $logsPath . '/' . $file;
+		// check if requested file is really in the logs directory
+		if (dirname($file) !== $logsPath) {
+			throw new \Exception('No such log file');
+		}
+		return $file;
+	}
 
-                        $log[] = array(
-                            'context' => $current[3],
-                            'level' => $level,
-                            'folder' => $this->folder,
-                            'level_class' => $this->level->cssClass($level),
-                            'level_img' => $this->level->img($level),
-                            'date' => $current[1],
-                            'text' => $current[4],
-                            'in_file' => isset($current[5]) ? $current[5] : null,
-                            'stack' => preg_replace("/^\n*/", '', $log_data[$i])
-                        );
-                    }
-                }
-            }
-        }
+	/**
+	 * @return string
+	 */
+	public function getFolderName()
+	{
+		return $this->folder;
+	}
 
-        if (empty($log)) {
+	/**
+	 * @return string
+	 */
+	public function getFileName()
+	{
+		return basename($this->file);
+	}
 
-            $lines = explode(PHP_EOL, $file);
-            $log = [];
+	/**
+	 * @return array
+	 */
+	public function all()
+	{
+		$log = [];
 
-            foreach ($lines as $key => $line) {
-                $log[] = [
-                    'context' => '',
-                    'level' => '',
-                    'folder' => '',
-                    'level_class' => '',
-                    'level_img' => '',
-                    'date' => $key + 1,
-                    'text' => $line,
-                    'in_file' => null,
-                    'stack' => '',
-                ];
-            }
-        }
+		if (!$this->file) {
+			$log_file = (!$this->folder) ? $this->getFiles() : $this->getFolderFiles();
+			if (!count($log_file)) {
+				return [];
+			}
+			$this->file = $log_file[0];
+		}
 
-        return array_reverse($log);
-    }
+		$max_file_size = function_exists('config') ? config('logviewer.max_file_size', self::MAX_FILE_SIZE) : self::MAX_FILE_SIZE;
+		if (app('files')->size($this->file) > $max_file_size) {
+			return null;
+		}
 
-    /**
-     * @return array
-     */
-    public function getFolders()
-    {
-        $folders = glob($this->storage_path . '/*', GLOB_ONLYDIR);
-        if (is_array($this->storage_path)) {
-            foreach ($this->storage_path as $value) {
-                $folders = array_merge(
-                    $folders,
-                    glob($value . '/*', GLOB_ONLYDIR)
-                );
-            }
-        }
+		$file = app('files')->get($this->file);
+		if (!preg_match_all($this->pattern->getPattern('log_entry'), $file, $log_entries, PREG_SET_ORDER)) {
+			dd('die');
+		}
 
-        if (is_array($folders)) {
-            foreach ($folders as $k => $folder) {
-                $folders[$k] = basename($folder);
-            }
-        }
-        return array_values($folders);
-    }
+		foreach ($log_entries as $log_entry) {
 
-    /**
-     * @param bool $basename
-     * @return array
-     */
-    public function getFolderFiles($basename = false)
-    {
-        return $this->getFiles($basename, $this->folder);
-    }
+			$context = $this->formatLogValue($log_entry[2]);
+			$level = $this->formatLogLevel($log_entry[3]);
+			$text = $this->formatText($log_entry[4]);
+			$stack_trace = $this->formatStackTrace($log_entry[5]);
+			$date = $this->formatLogDate($log_entry[1]);
 
-    /**
-     * @param bool $basename
-     * @param string $folder
-     * @return array
-     */
-    public function getFiles($basename = false, $folder = '')
-    {
-        $pattern = function_exists('config') ? config('logviewer.pattern', '*.log') : '*.log';
-        $files = glob(
-            $this->storage_path . '/' . $folder . '/' . $pattern,
-            preg_match($this->pattern->getPattern('files'), $pattern) ? GLOB_BRACE : 0
-        );
-        if (is_array($this->storage_path)) {
-            foreach ($this->storage_path as $value) {
-                $files = array_merge(
-                  $files,
-                  glob(
-                      $value . '/' . $folder . '/' . $pattern,
-                      preg_match($this->pattern->getPattern('files'), $pattern) ? GLOB_BRACE : 0
-                  )
-                );
-            }
-        }
+			$log[] = array(
+				'context' => $context,
+				'level' => $level,
+				'folder' => $this->folder,
+				'level_class' => $this->level->cssClass($level),
+				'level_img' => $this->level->img($level),
+				'date' => $log_entry[1],
+				'text' => $text,
+				'in_file' =>  $date,
+				'stack' => $stack_trace,
+			);
+		}
 
-        $files = array_reverse($files);
-        $files = array_filter($files, 'is_file');
-        if ($basename && is_array($files)) {
-            foreach ($files as $k => $file) {
-                $files[$k] = basename($file);
-            }
-        }
-        return array_values($files);
-    }
+		if (empty($log)) {
+
+			$lines = explode(PHP_EOL, $file);
+			$log = [];
+
+			foreach ($lines as $key => $line) {
+				$log[] = [
+					'context' => '',
+					'level' => '',
+					'folder' => '',
+					'level_class' => '',
+					'level_img' => '',
+					'date' => $key + 1,
+					'text' => $line,
+					'in_file' => null,
+					'stack' => '',
+				];
+			}
+		}
+
+		return array_reverse($log);
+	}
+
+	/**
+	 * @return array
+	 */
+	public function getFolders()
+	{
+		$folders = glob($this->storage_path . '/*', GLOB_ONLYDIR);
+		if (is_array($this->storage_path)) {
+			foreach ($this->storage_path as $value) {
+				$folders = array_merge(
+					$folders,
+					glob($value . '/*', GLOB_ONLYDIR)
+				);
+			}
+		}
+
+		if (is_array($folders)) {
+			foreach ($folders as $k => $folder) {
+				$folders[$k] = basename($folder);
+			}
+		}
+		return array_values($folders);
+	}
+
+	/**
+	 * @param bool $basename
+	 * @return array
+	 */
+	public function getFolderFiles($basename = false)
+	{
+		return $this->getFiles($basename, $this->folder);
+	}
+
+	/**
+	 * @param bool $basename
+	 * @param string $folder
+	 * @return array
+	 */
+	public function getFiles($basename = false, $folder = '')
+	{
+		$pattern = function_exists('config') ? config('logviewer.pattern', '*.log') : '*.log';
+		$files = glob(
+			$this->storage_path . '/' . $folder . '/' . $pattern,
+			preg_match($this->pattern->getPattern('files'), $pattern) ? GLOB_BRACE : 0
+		);
+		if (is_array($this->storage_path)) {
+			foreach ($this->storage_path as $value) {
+				$files = array_merge(
+					$files,
+					glob(
+						$value . '/' . $folder . '/' . $pattern,
+						preg_match($this->pattern->getPattern('files'), $pattern) ? GLOB_BRACE : 0
+					)
+				);
+			}
+		}
+
+		$files = array_reverse($files);
+		$files = array_filter($files, 'is_file');
+		if ($basename && is_array($files)) {
+			foreach ($files as $k => $file) {
+				$files[$k] = basename($file);
+			}
+		}
+		return array_values($files);
+	}
 }
--- src/Rap2hpoutre/LaravelLogViewer/Pattern.php
+++ src/Rap2hpoutre/LaravelLogViewer/Pattern.php
@@ -10,38 +10,42 @@ namespace Rap2hpoutre\LaravelLogViewer;
 
 class Pattern
 {
+	private $datetime_pattern = '(\d{4}-\d{2}-\d{2}\s+\d{2}:\d{2}:\d{2})';
+	private $context_pattern = '(\w+)';
+	private $level_pattern = '(\w+)';
+	private $error_text_pattern = '(.+?(?={"\w))';
+	private $metadata_pattern = '(.+?(?=\n\[\d|\s*\Z))';
 
-    /**
-     * @var array
-     */
-    private $patterns = [
-        'logs' => '/\[\d{4}-\d{2}-\d{2} \d{2}:\d{2}:\d{2}([\+-]\d{4})?\].*/',
-        'current_log' => [
-            '/^\[(\d{4}-\d{2}-\d{2} \d{2}:\d{2}:\d{2}([\+-]\d{4})?)\](?:.*?(\w+)\.|.*?)',
-            ': (.*?)( in .*?:[0-9]+)?$/i'
-        ],
-        'files' => '/\{.*?\,.*?\}/i',
-    ];
+	/**
+	 * @var array
+	 */
+	private function getPatterns()
+	{
+		return [
+			'log_entry' => '~^\[' . $this->datetime_pattern . '\]\s+' . $this->context_pattern . '.' . $this->level_pattern . ':\s+' . $this->error_text_pattern . $this->metadata_pattern . '~ismu',
+			'files' => '/\{.*?\,.*?\}/i',
+			'stack_line' => '~#(\d+)\s+(?:(\[internal function\])|(.+?\.php)\((\d+)\)):\s+(.+)~',
+		];
+	}
 
-    /**
-     * @return array
-     */
-    public function all()
-    {
-        return array_keys($this->patterns);
-    }
+	/**
+	 * @return array
+	 */
+	public function all()
+	{
+		return array_keys($this->getPatterns());
+	}
 
-    /**
-     * @param $pattern
-     * @param null $position
-     * @return string pattern
-     */
-    public function getPattern($pattern, $position = null)
-    {
-        if ($position !== null) {
-            return $this->patterns[$pattern][$position];
-        }
-        return $this->patterns[$pattern];
-        
-    }
+	/**
+	 * @param $pattern
+	 * @param null $position
+	 * @return string pattern
+	 */
+	public function getPattern($pattern, $position = null)
+	{
+		if ($position !== null) {
+			return $this->getPatterns()[$pattern][$position];
+		}
+		return $this->getPatterns()[$pattern];
+	}
 }
