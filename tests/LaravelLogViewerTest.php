0232f5f63329df2b3daadac9f34bbbec8fd2d3de
Николай Митин <nick.mitin@gmail.com>
2020-03-06 07:19:43 +0300

    VS Code formatting

--- tests/LaravelLogViewerTest.php
+++ tests/LaravelLogViewerTest.php
@@ -11,48 +11,47 @@ use Orchestra\Testbench\TestCase as OrchestraTestCase;
 class LaravelLogViewerTest extends OrchestraTestCase
 {
 
-    public function setUp()
-    {
-        parent::setUp();
-        // Copy "laravel.log" file to the orchestra package.
-        if (!file_exists(storage_path() . '/logs/laravel.log')) {
-            copy(__DIR__ . '/laravel.log', storage_path() . '/logs/laravel.log');
-        }
-    }
+	public function setUp()
+	{
+		parent::setUp();
+		// Copy "laravel.log" file to the orchestra package.
+		if (!file_exists(storage_path() . '/logs/laravel.log')) {
+			copy(__DIR__ . '/laravel.log', storage_path() . '/logs/laravel.log');
+		}
+	}
 
-    /**
-     * @throws \Exception
-     */
-    public function testSetFile()
-    {
-        parent::setUp();
+	/**
+	 * @throws \Exception
+	 */
+	public function testSetFile()
+	{
+		parent::setUp();
 
-        $laravel_log_viewer = new LaravelLogViewer();
-        try {
-            $laravel_log_viewer->setFile("laravel.log");
-        } catch (\Exception $e) {
-            throw new \Exception($e->getMessage());
-        }
+		$laravel_log_viewer = new LaravelLogViewer();
+		try {
+			$laravel_log_viewer->setFile("laravel.log");
+		} catch (\Exception $e) {
+			throw new \Exception($e->getMessage());
+		}
 
-        $this->assertEquals("laravel.log", $laravel_log_viewer->getFileName());
-    }
+		$this->assertEquals("laravel.log", $laravel_log_viewer->getFileName());
+	}
 
-    public function testAll()
-    {
-        $laravel_log_viewer = new LaravelLogViewer();
-        $data = $laravel_log_viewer->all();
-        $this->assertEquals('local', $data[0]['context']);
-        $this->assertEquals('error', $data[0]['level']);
-        $this->assertEquals('danger', $data[0]['level_class']);
-        $this->assertEquals('exclamation-triangle', $data[0]['level_img']);
-        $this->assertEquals('2018-09-05 20:20:51', $data[0]['date']);
-    }
+	public function testAll()
+	{
+		$laravel_log_viewer = new LaravelLogViewer();
+		$data = $laravel_log_viewer->all();
+		$this->assertEquals('local', $data[0]['context']);
+		$this->assertEquals('error', $data[0]['level']);
+		$this->assertEquals('danger', $data[0]['level_class']);
+		$this->assertEquals('exclamation-triangle', $data[0]['level_img']);
+		$this->assertEquals('2018-09-05 20:20:51', $data[0]['date']);
+	}
 
-    public function testGetFolderFiles()
-    {
-        $laravel_log_viewer = new LaravelLogViewer();
-        $data = $laravel_log_viewer->getFolderFiles();
-        $this->assertNotEmpty($data[0], "Folder files is null");
-    }
-
+	public function testGetFolderFiles()
+	{
+		$laravel_log_viewer = new LaravelLogViewer();
+		$data = $laravel_log_viewer->getFolderFiles();
+		$this->assertNotEmpty($data[0], "Folder files is null");
+	}
 }
